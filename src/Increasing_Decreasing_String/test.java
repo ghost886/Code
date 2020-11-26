9d3e8111573680dec112c9b111ad7974cd651278
ZMX <1194236582@qq.com>
2020-11-26 19:15:59 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/Increasing_Decreasing_String/test.java
@@ -0,0 +1,12 @@
+package Increasing_Decreasing_String;
+
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test(){
+        Solution solution=new Solution();
+        System.out.println(solution.sortString("sdkjfkheogrhgieurhgiua"));
+    }
+
+}
---
+++ src/Roman_to_Int/test.java
@@ -0,0 +1,11 @@
+package Roman_to_Int;
+
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test(){
+        Solution solution=new Solution();
+        System.out.println(solution.romanToInt("CMXCIX"));
+    }
+}
