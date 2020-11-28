a378a6f87512080d84b3853cc04321e4065af0c9
ghost886 <1194236582@qq.com>
2020-11-28 11:28:20 +0800

    2020/11/28没有完成的以到累加树的题目

---
+++ src/Convert_BST_to_Greater_Tree/TreeNode.java
@@ -0,0 +1,22 @@
+package Convert_BST_to_Greater_Tree;
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+     TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
