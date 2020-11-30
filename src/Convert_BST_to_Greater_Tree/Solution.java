c7700e1b2083e5d03ddb791163a5d0c4572a8fcc
ghost886 <1194236582@qq.com>
2020-11-30 23:13:44 +0800

    2020/11/30没有完成的以到累加树的题目

--- src/Convert_BST_to_Greater_Tree/Solution.java
+++ src/Convert_BST_to_Greater_Tree/Solution.java
@@ -2,19 +2,20 @@ package Convert_BST_to_Greater_Tree;
 class Solution {
     //这道题目仔细想想是有难度的
     //通过后序遍历，可以实现
+    //仅仅加上右子树的值时没有用的
+    //所以中序遍历吧
+    int sum=0;
     public TreeNode convertBST(TreeNode root) {
-        right_val(0,root,false);
+        traverse(root);
         return root;
     }
-    public int right_val(int parent_val,TreeNode node,boolean right){
-        //返回以node的那棵树的所有数相加
-        if(node==null)
-            return 0;
-        if(right)
-            node.val=node.val+right_val(0,node.right,true);
-        else
-            node.val=parent_val+node.val+right_val(0,node.right,true);
-        right_val(node.val, node.left,false);
-        return node.val;
+    void traverse(TreeNode root){
+        if(root==null){
+            return;
+        }
+        traverse(root.right);
+        sum+=root.val;
+        root.val=sum;
+        traverse(root.left);
     }
 }
