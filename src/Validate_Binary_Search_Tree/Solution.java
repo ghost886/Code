1942a5e1b71eb317227f74c46fecf9182bfa6e7c
ghost886 <1194236582@qq.com>
2020-12-06 22:47:30 +0800

    2020/12/7 验证二叉搜索树

---
+++ src/Validate_Binary_Search_Tree/Solution.java
@@ -0,0 +1,28 @@
+package Validate_Binary_Search_Tree;
+
+public class Solution {
+    public boolean first_time=true;
+    public int before;
+    boolean res=true;
+    public boolean isValidBST(TreeNode root) {
+        mid(root);
+        return res;
+    }
+    public void mid(TreeNode root){
+        if(root==null)
+            return;
+        else{
+            mid(root.left);
+            if(first_time) {
+                first_time=false;
+                before = root.val;
+            }
+            else{
+                if(before>=root.val)
+                    res=false;
+                before=root.val;
+            }
+            mid(root.right);
+        }
+    }
+}
